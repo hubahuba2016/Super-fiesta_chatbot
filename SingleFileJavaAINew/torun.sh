d020b24433289eaf183fa2df41c1119d2400984e
Hubaka Ghayati Setyakurnia <hubakag@gmail.com>
2025-07-31 12:44:57 +0700

    log bug fix

---
+++ SingleFileJavaAINew/torun.sh
@@ -0,0 +1,5 @@
+javac -cp ".:commons-math3-3.6.1.jar:sqlite-jdbc-3.43.2.2.jar:slf4j-api-1.7.36.jar:slf4j-simple-1.7.36.jar" Chatbot.java
+
+java -cp ".:commons-math3-3.6.1.jar:sqlite-jdbc-3.43.2.2.jar:slf4j-api-1.7.36.jar:slf4j-simple-1.7.36.jar" Chatbot
+
+#just copy paste the commands if it's not working
